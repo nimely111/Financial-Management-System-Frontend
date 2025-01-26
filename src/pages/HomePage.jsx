9d8d157bbbb86dbafe6e95da10e4ca2447690b29
Nimely111 <samuelehnimely@gmail.com>
2025-01-26 10:23:36 +0000

    update comment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
-import Hero from '../components/Hero';
-import HomeCards from '../components/HomeCards';
-import UserListings from '../components/UserListings';
-import ViewAllUsers from '../components/ViewAllUsers';
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import UserListings from "../components/UserListings";
+import ViewAllUsers from "../components/ViewAllUsers";
 
 const HomePage = () => {
   return (
-    // place all components in the homepage nstead of the app.jsx file
+    // place all components in the homepage nestead of the app.jsx file
     <>
       <Hero />
       <HomeCards />
