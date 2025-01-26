0829b6b60eb7cce6cd9b1cc5fffabb037fbbe003
Nimely111 <samuelehnimely@gmail.com>
2025-01-26 10:21:06 +0000

    removed unnessary comment from the add recorn form

--- src/components/AddRecordForm.jsx
+++ src/components/AddRecordForm.jsx
@@ -1,52 +1,51 @@
-// src/components/AddRecordForm.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddRecordForm = ({ addRecord }) => {
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState("");
   const [isPaid, setIsPaid] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (date) {
       addRecord({ date, isPaid });
-      setDate('');
+      setDate("");
       setIsPaid(false);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='mb-4'>
-        <label htmlFor='date' className='block text-gray-700 font-bold mb-2'>
+      <div className="mb-4">
+        <label htmlFor="date" className="block text-gray-700 font-bold mb-2">
           Select Date
         </label>
         <input
-          type='date'
-          id='date'
+          type="date"
+          id="date"
           value={date}
           onChange={(e) => setDate(e.target.value)}
-          className='border rounded w-full py-2 px-3'
+          className="border rounded w-full py-2 px-3"
         />
       </div>
 
-      <div className='mb-4'>
-        <label className='block text-gray-700 font-bold mb-2'>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2">
           Payment Status
         </label>
         <input
-          type='checkbox'
-          id='isPaid'
+          type="checkbox"
+          id="isPaid"
           checked={isPaid}
           onChange={(e) => setIsPaid(e.target.checked)}
         />
-        <label htmlFor='isPaid' className='ml-2'>
+        <label htmlFor="isPaid" className="ml-2">
           Paid
         </label>
       </div>
 
       <button
-        type='submit'
-        className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full'
+        type="submit"
+        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full"
       >
         Add Record
       </button>
