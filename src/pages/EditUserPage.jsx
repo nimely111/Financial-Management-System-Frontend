9bd6e4ae1cba4a0fb2a102db7ba49d38d3ee2263
Nimely111 <samuelehnimely@gmail.com>
2025-01-27 23:20:09 +0000

    rename city address to address

--- src/pages/EditUserPage.jsx
+++ src/pages/EditUserPage.jsx
@@ -124,7 +124,7 @@ const EditUserPage = ({ updateUserSubmit }) => {
 
             <div className="mb-4">
               <label className="block text-gray-700 font-bold mb-2">
-                City Address
+                Address
               </label>
               <input
                 type="text"
