fdf3f11df74256cb9e2c78ea10019c770e5c03c5
Nimely111 <samuelehnimely@gmail.com>
2025-01-31 21:36:26 +0000

    update

--- src/pages/AddUserPage.jsx
+++ src/pages/AddUserPage.jsx
@@ -78,8 +78,8 @@ const AddUserPage = ({ addUserSubmit }) => {
               </label>
               <select
                 id="savings_type"
-                name="                id="savings_type"
-"
+                name="savings_type"
+                id="savings_type"
                 className="border rounded w-full py-2 px-3"
                 value={savingsType}
                 onChange={(e) => setSavingsType(e.target.value)}
