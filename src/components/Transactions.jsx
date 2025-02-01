c8e3d2def29fc51b48401a5640b9b19df49a1304
Nimely111 <samuelehnimely@gmail.com>
2025-02-01 23:54:36 +0000

    add transactions maping logic to a seperate component

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,5 +1,15 @@
 const Transactions = () => {
-  return <section></section>;
+  return (
+    <>
+      {user.transactions.length > 0 ? (
+        user.transactions.map((transaction, index) => (
+          <p key={index}>{transaction.savings_amount}</p>
+        ))
+      ) : (
+        <p>N/A</p>
+      )}
+    </>
+  );
 };
 
 export default Transactions;
