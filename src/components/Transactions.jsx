97e2fc49ffb37c0d6856e94a406ebe982de4eaf7
Nimely111 <samuelehnimely@gmail.com>
2025-02-02 13:34:18 +0000

    Refactor Transactions component to accept transactions as a prop

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,31 +1,12 @@
-import { BACKEND_URL } from "../api";
 import { useState, useEffect } from "react";
-const Transactions = () => {
-  const user = `${BACKEND_URL}/users`;
+import { BACKEND_URL } from "../api";
+import { toast } from "react-toastify";
 
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch users: ${response.statusText}`);
-      }
-
-      const data = await response.json();
-      setUsers(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      toast.error("Failed to load users. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  fetchUsers();
-
+const Transactions = ({ transactions }) => {
   return (
     <>
-      {user.transactions.length > 0 ? (
-        user.transactions.map((transaction, index) => (
+      {transactions.length > 0 ? (
+        transactions.map((transaction, index) => (
           <p key={index}>{transaction.savings_amount}</p>
         ))
       ) : (
