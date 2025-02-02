08deeaef10ac7c8bd627db7e9aca09f567b42d02
Nimely111 <samuelehnimely@gmail.com>
2025-02-02 21:15:52 +0000

    revert changes to previous structure

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -5,15 +5,13 @@ import { toast } from "react-toastify";
 const Transactions = ({ transactions }) => {
   return (
     <>
-      <div>
-        {transactions.length > 0 ? (
-          transactions.map((transaction, index) => (
-            <p key={index}>{transaction.savings_type}</p>
-          ))
-        ) : (
-          <p>N/A</p>
-        )}
-      </div>
+      {transactions.length > 0 ? (
+        transactions.map((transaction, index) => (
+          <p key={index}>{transaction.savings_amount}</p>
+        ))
+      ) : (
+        <p>N/A</p>
+      )}
     </>
   );
 };
